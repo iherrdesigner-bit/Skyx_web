fd1c70ad21165ec2926aeb3cbecb70b537bd4008
iherrdesigner-bit <iherr.designer@gmail.com>
2025-08-22 21:48:21 +0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE_NAME='skyx-cache-v5';
+const CACHE_NAME='skyx-cache-v6';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.json','./assets/icon-192.png','./assets/icon-512.png','./assets/apple-touch-icon.png','./assets/Skyx_Favicon.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME?caches.delete(k):null))))});
